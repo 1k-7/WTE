7597743df2fb528cdd927b3cc6eed3b570dd2da1
Aldrin Rishi <perfect.kryptonian@gmail.com>
2025-09-23 20:33:11 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,6 +17,7 @@ echo "Setting permissions for browser executables..."
 chmod -R 755 $PLAYWRIGHT_BROWSERS_PATH
 
 # (Optional) List the contents to verify in the logs that the files exist and are executable
+echo "Verifying installation..."
 ls -laR $PLAYWRIGHT_BROWSERS_PATH
 
 echo "Build script finished."
