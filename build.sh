51091b114331d32a1e797d11e11b8c2c231533a3
Aldrin Rishi <perfect.kryptonian@gmail.com>
2025-09-23 19:46:42 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@ set -o errexit
 # Install pip dependencies
 pip install -r requirements.txt
 
+# Set a persistent location for Playwright's browser cache within the project directory
+export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/.playwright
+
 # Explicitly run playwright's install command using the python from the venv
 python -m playwright install --with-deps chromium
